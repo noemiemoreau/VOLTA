100ee1fd3457f886ebb76ebc92d4305155440593
Celestenono <noemie.moreau96@gmail.com>
2024-03-20 17:57:50 +0100

    correct code

--- main.py
+++ main.py
@@ -442,9 +442,12 @@ def main_worker(gpu, ngpus_per_node, config, reporter):
         if (config['validation_interval'] != 0) and (epoch % config['validation_interval'] == 0 or last_epoch):
             val_nn_acc, val_kmeans_metric, val_standalone_kmeans_metric, (val_embedding, val_labels) = \
                 validation(model, train_loader, val_loader, config)
-            config['logger'].info(
-                f"iteration {epoch} val_nn_acc: {val_nn_acc:.4f}, val_kmeans_metric: {val_kmeans_metric:.4f}, "
-                f"val_standalone_kmeans_metric: {val_standalone_kmeans_metric:.4f}")
+            print(val_nn_acc)
+            print(val_kmeans_metric)
+            print(val_standalone_kmeans_metric)
+            # config['logger'].info(
+            #     f"iteration {epoch} val_nn_acc: {val_nn_acc:.4f}, val_kmeans_metric: {val_kmeans_metric:.4f}, "
+            #     f"val_standalone_kmeans_metric: {val_standalone_kmeans_metric:.4f}")
 
             if not config['multiprocessing_distributed'] or config['rank'] % ngpus_per_node == 0:
                 is_best = val_nn_acc > best_acc1
