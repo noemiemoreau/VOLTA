00f69aec72ef106a944c28fe0dcb0d5f7b39c765
Celestenono <noemie.moreau96@gmail.com>
2024-03-21 15:12:13 +0100

    correct code

--- main.py
+++ main.py
@@ -397,7 +397,7 @@ def main_worker(gpu, ngpus_per_node, config, reporter):
     cudnn.benchmark = True
 
     # Data loading code
-    train_dir = os.path.join(config['data'], 'val')
+    train_dir = os.path.join(config['data'], 'train')
     #todo change this
     val_dir = os.path.join(config['data'], 'val')
 
