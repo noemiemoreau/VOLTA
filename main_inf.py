68369313968d4bd868ac360331792e7be9269011
Celestenono <noemie.moreau96@gmail.com>
2024-04-05 15:32:22 +0200

    code for nucls

--- main_inf.py
+++ main_inf.py
@@ -387,7 +387,7 @@ def main_worker(gpu, ngpus_per_node, config, reporter):
     # Data loading code
     train_dir = os.path.join(config['data'], 'train_cells')
     #todo change this
-    test_dir = os.path.join(config['data'], 'test_cells')
+    test_dir = os.path.join(config['data'], 'val_cells')
 
     # -------------------------------- dataset -------------------------------
     train_dataset, test_dataset = get_dataset(config, test_dir, train_dir)
