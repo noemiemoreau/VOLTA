c46828f1fd33b0a4a1f786792111ce8c6244e39d
Celestenono <noemie.moreau96@gmail.com>
2025-06-11 12:08:29 +0200

    fix for new torch version

--- dataset/kidney/dataset.py
+++ dataset/kidney/dataset.py
@@ -239,8 +239,7 @@ class KidneyDataset(Dataset):
             # Note: convert mask to float and numpy because of the albumentation
             masks = [mask.float().numpy(), segmentation]
             # masks = [mask, segmentation]
-            print(patch)
-            patch = self.patch_transform(image=np.array(patch))
+            patch = self.patch_transform(image=np.array(patch), masks=np.array(mask))
             if isinstance(patch, dict):  # return output of the albumentation transform
                 mask, segmentation = patch['masks']
                 mask, segmentation = np.array(mask).astype(bool), np.array(segmentation).astype(bool)
